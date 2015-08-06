52cb635f8aa1657130144da9992e06caaff67853
Martin Hering <hering@vemedio.com>
2015-08-06 10:36:28 +0200

    added ZipArchiveKit OSX framework target

---
+++ ZipArchive/ZipArchiveKit/ZipArchiveKit.h
@@ -0,0 +1,20 @@
+//
+//  ZipArchiveKit.h
+//  ZipArchiveKit
+//
+//  Created by Martin Hering on 06.08.15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZipArchiveKit.
+FOUNDATION_EXPORT double ZipArchiveKitVersionNumber;
+
+//! Project version string for ZipArchiveKit.
+FOUNDATION_EXPORT const unsigned char ZipArchiveKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZipArchiveKit/PublicHeader.h>
+
+
+#import <ZipArchiveKit/ZipArchive.h>
